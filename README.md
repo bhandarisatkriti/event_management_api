18d19b86cd55e0a2b6e289a935467639d4b1f796
bhandarisatkriti <satkriti32@gmail.com>
2026-06-10 22:18:08 +0545

    Initial Django Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Event Management API
+
+A Django REST Framework backend project for managing events, organizers, and user registrations.
+
---
+++ events/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Organizer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    venue = models.CharField(max_length=200)
+    start_time = models.DateField()
+    end_time = models.DateField()
+    banner = models.ImageField(upload_to="banners/")
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+    
+class Registration(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    event = models.ForeignKey(Event, on_delete = models.CASCADE)
+    registered_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
